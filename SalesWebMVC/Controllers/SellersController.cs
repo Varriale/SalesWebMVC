6f64554f0221a594d7fdd70a675b149477695626
Eduardo Varriale da Silva <eduardo.silva@altus.com.br>
2021-10-19 14:08:17 -0300

    Sellers Controller

---
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public SellersController(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
