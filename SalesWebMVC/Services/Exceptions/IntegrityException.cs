03160297e9e1db6a6739fef342a9955fc669743f
Eduardo Varriale da Silva <eduardo.silva@altus.com.br>
2021-10-20 10:22:24 -0300

    IntegrityException handling for deletion

---
+++ SalesWebMVC/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SalesWebMVC.Services.Exceptions
+{
+    public class IntegrityException : Exception
+    {
+        public IntegrityException(string message) : base(message)
+        {
+        }
+    }
+}
